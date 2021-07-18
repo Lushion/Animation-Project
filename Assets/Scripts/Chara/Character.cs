6da0159eb06539324cecc65dce364abc7124f2b6
Lushion <kphan.xm@gmail.com>
2021-07-18 13:20:16 +0900

    No character Rigging package2

--- Assets/Scripts/Chara/Character.cs
+++ Assets/Scripts/Chara/Character.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Animations.Rigging;
 using UnityEngine.UI;
 
 public class Character : MonoBehaviour
